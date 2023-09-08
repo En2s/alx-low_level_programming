da8c2f499a2609ed052e4121913a01fbedb78202
En2s <enasbdraldeen65@gmail.com>
2023-09-08 07:06:17 -0700

    Fix 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
-#include <stdio.h> 
-  
- /** 
-  * main - Entry point 
-  * 
-  * Description: prints "with proper grammar, but the outcome is a piece of 
-  * art," 
-  * 
-  * Return: Always 0 (Success) 
-  */ 
- int main(void) 
- { 
-         printf("with proper grammar, but the outcome is a piece of art,\n"); 
-         return (0); 
- }
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Hello, puts'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
