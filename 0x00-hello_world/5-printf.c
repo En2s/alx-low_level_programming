81b32fe3b4b395d62105d0b9ca95d31a0b502f2b
En2s <enasbdraldeen65@gmail.com>
2023-09-08 07:01:16 -0700

    Add lost files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Hello, printf'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
