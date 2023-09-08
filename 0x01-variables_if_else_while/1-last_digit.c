0914c4b6c1fa3b537588579c0c2182ea677cda72
En2s <enasbdraldeen65@gmail.com>
2023-09-08 11:58:57 -0700

    My first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,32 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - print the last digit of the number stored in variable n
- *
- * Description: program will assign a random number to the variable n each time
- * it is executed.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d ", n, n % 10);
-	if (n % 10 > 5)
-	{
-		printf("and is greater than 5\n");
-	}
-	if (n % 10 == 0)
-	{
-		printf("and is 0\n");
-	}
-	if (n % 10 != 0 && n % 10 < 6)
-	{
-		printf("and is less than 6 and not 0\n");
-	}
-	return (0);
-}
+#include <stdio.h> 
+ #include <stdlib.h> 
+ #include <time.h> 
+  
+ /** 
+  * main - print last digit in random number 
+  * 
+  * Description: print last digit in random number 
+  * 
+  * Return: Always 0 (Success) 
+  */ 
+  
+ int main(void) 
+ { 
+         int n, last; 
+  
+         srand(time(0)); 
+         n = rand() - RAND_MAX / 2; 
+         last = n % 10; 
+         if (last > 5) 
+                 printf("Last digit of %d is %d and is greater than 5\n", n, last); 
+         else if (last == 0) 
+                 printf("Last digit of %d is %d and is 0\n", n, last); 
+         else if (last < 6 && last != 0) 
+                 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last); 
+         return (0); 
+ }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - a program that prints the alphabet in
- * lowercase, and then in uppercase
- *
- * Description: a program that prints the alphabet
- * in lowercase, and then in uppercase
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; ++letter)
-	{
-		putchar(letter);
-	}
-	putchar('\n');
-	return (0);
-}
+#include <stdio.h> 
+  
+ /** 
+  * main - print the letters of the alphabet 
+  * 
+  * Description: print the letters of the alphabet 
+  * 
+  * Return: Always 0 (Success) 
+  */ 
+  
+ int main(void) 
+ { 
+         int i = 97; 
+  
+         while (i < 123) 
+         { 
+                 putchar(i); 
+                 i++; 
+         } 
+         putchar(10); 
+  
+         return (0); 
+ }
